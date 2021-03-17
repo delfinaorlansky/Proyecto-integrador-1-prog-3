603f2904a2a62c7ddd98f30d701001a764aaa077
Delfina Orlansky <dorlansky@udesa.edu.ar>
2021-03-17 17:47:40 -0300

    hola

---
+++ mi-primer-react/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+     <h1>Este es un proyecto hehco con React</h1>
+     <p> Tener en cuenta que todo lo que escribamos aca es el contenido </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ mi-primer-react/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//El primer paramentro es que quiero enviar y el segundo parametro es a donde
+ReactDOM.render(<App />, document.getElementById('root'));
+
